3b55ffd5eaec872767c0f9514eab24351b1d0823
Atul <102244373+Atul-837@users.noreply.github.com>
2022-05-24 12:43:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -120,9 +120,9 @@ class Destination {
     const markup = `
     <div class="container-fluid destination">
     <div class="slide">
-    <button class="bi bi-chevron-left pic-icon">&#xF284</button>
+    <button><i class="bi bi-chevron-left pic-icon"></i></button>
     <div class='photos d-flex'></div>
-    <button class="bi bi-chevron-right pic-icon">&#xF285</button>
+    <button><i class="bi bi-chevron-right pic-icon"></i></button>
     </div>
     <div class='dots container-fluid'></div>
     <p>${this.destination}<p>
